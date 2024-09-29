5730e404ff7261729505623070b7383404001553
csxark <arktandoncs@gmail.com>
2024-09-30 01:54:31 +0530

    new project 9

---
+++ 09_Project/script.js
@@ -0,0 +1,29 @@
+let intervalid;
+const randomcolor = function () {
+    const hex ='0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+        }
+    return color;
+}
+
+const startchangecolor =function () {
+
+    function changebgcolor() {
+        document.body.style.backgroundColor=randomcolor();
+    };
+    if (!intervalid) {
+        intervalid=setInterval(changebgcolor, 100);
+    };
+};
+
+const stopchangecolor = function () {
+    clearInterval(intervalid);
+    intervalid=null;
+};
+
+document.querySelector("#start").addEventListener('click',startchangecolor);
+document.querySelector("#stop").addEventListener('click',stopchangecolor);
+
+
