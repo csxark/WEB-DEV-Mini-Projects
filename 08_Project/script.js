933060e442a6e03e1c52a3c85fd3d5c288dd6340
csxark <arktandoncs@gmail.com>
2024-10-21 01:11:46 +0530

    Update script.js

---
+++ 08_Project/script.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+const yesno = document.querySelector('.yesno');
+const guessslot = document.querySelector('.guesses');
+const startover = document.querySelector('.resultParas');
+const remaining = document.querySelector('.lastResult');
+const p = document.createElement('p');
+const color_array = ["red", "blue", "lime", "yellow", "purple", "orange", "black", "pink", "white", "gray", "aqua"];
